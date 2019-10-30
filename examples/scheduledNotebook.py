e6b3d1197eb6ea686587b56500d47c9b46d3bac6
Michael Helbo Nygaard <mnygaard@grundfos.com>
2019-10-30 15:11:24 +0100

    [MHEN] Updated scheduler

--- examples/scheduledNotebook.py
+++ examples/scheduledNotebook.py
@@ -1,9 +1,14 @@
 import schedule
 import time
-import papermill
+import papermill as pm
 
 def job(): # parameterize job - naturally!
-    papermill OverflowServiceSandbox.ipynb ./OverflowServiceSandbox_run_cli.ipynb -p start '2019-06-04T0:55:52Z' -p stop '2019-06-05T19:28:52Z' -p levelThreshold 0.45 -p maxThreshold 0.90 -p levelSlopeAngle 0.000085 -p dataOffset 0 -p bufferLength 30 -p resultAttribute 'overflow'
+    pm.execute_notebook(
+		'OverflowServiceSandbox.ipynb',
+		'./OverflowServiceSandbox_run_cli.ipynb',
+		parameters = dict(start='2019-06-04T0:55:52Z', stop='2019-06-05T19:28:52Z', levelThreshold=0.45, maxThreshold=0.90, levelSlopeAngle=0.000085, dataOffset=0, bufferLength=30, resultAttribute='overflow')
+	)
+#papermill OverflowServiceSandbox.ipynb ./OverflowServiceSandbox_run_cli.ipynb -p start '2019-06-04T0:55:52Z' -p stop '2019-06-05T19:28:52Z' -p levelThreshold 0.45 -p maxThreshold 0.90 -p levelSlopeAngle 0.000085 -p dataOffset 0 -p bufferLength 30 -p resultAttribute 'overflow'
 
 schedule.every(1).minutes.do(job)
 #schedule.every().hour.do(job)
