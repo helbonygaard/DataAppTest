28737514a1ddc3ba1c6ab8f52b3f20968c7fe417
Michael Helbo Nygaard <mnygaard@grundfos.com>
2019-10-30 11:37:05 +0100

    [MHEN] Updated AirFlow

--- examples/AirFlowDAG.py
+++ examples/AirFlowDAG.py
@@ -31,7 +31,7 @@ t1 = BashOperator(
 
 t2 = BashOperator(
     task_id='sleep',
-    bash_command='sleep 5',
+    bash_command='sleep 2',
     retries=3,
     dag=dag)
 	
@@ -49,9 +49,40 @@ t3 = BashOperator(
     params={'my_param': 'Parameter I passed in'},
     dag=dag)
 
+def print_context(ds, **kwargs):
+    pprint(kwargs)
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
+
+run_this = PythonOperator(
+    task_id='Sponge City Overflow Evaluation',
+    provide_context=True,
+    python_callable=print_context,
+    dag=dag,
+)
+
+def my_sleeping_function(random_base):
+    """This is a function that will run within the DAG execution"""
+    time.sleep(random_base)
+
+
+# Generate 5 sleeping tasks, sleeping from 0.0 to 0.4 seconds respectively
+for i in range(5):
+    task = PythonOperator(
+        task_id='sleep_for_' + str(i),
+        python_callable=my_sleeping_function,
+        op_kwargs={'random_base': float(i) / 10},
+        dag=dag,
+    )
+
+    run_this >> task
+
+
 # Setting up Dependencies
 t1.set_downstream(t2)
 
+"""
 # This means that t2 will depend on t1
 # running successfully to run.
 # It is equivalent to:
@@ -75,3 +106,4 @@ t1 >> t2 >> t3
 t1.set_downstream([t2, t3])
 t1 >> [t2, t3]
 [t2, t3] << t1
+"""
